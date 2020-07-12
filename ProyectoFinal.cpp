47a5fd7a01f3b1d1e6a3c28c2ff6648ac1bf641b
Lou2714 <00090220@uca.edu.sv>
2020-07-12 11:04:01 -0600

    Proyecto Final Factura

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -11,25 +11,24 @@ struct costoPorArticulo{
     float costoPorArt;
 }; 
 float PedirInformacion(int n, costoPorArticulo articulo[]){
-    string nombre;//Se declara la variable en que recibirá el nombre del artículo
+    string nombre;//Se declara la variable que recibirá el nombre del artículo
     for (int i = 0; i < n; i++)
     {
         cout<<"Nombre del articulo a pagar: ";
-        getline(cin,nombre, '\n');//Se emplea el getline por si se requiere un nombre con espacios, como Leche de soya
+        getline(cin,nombre, '\n');//Se emplea el getline por si se requiere un nombre con espacios, como por ejemplo: Leche de soya
         strncpy(articulo[i].nombreArticulo,nombre.c_str( ),longCad);/*Se copia el nombre ingresado en el campo del arreglo: articulo[i]
-        indicando que no debe de exceder 20 espacios*/
-        articulo[i].nombreArticulo[longCad]='\0';//el último espacio se guarda para el carácter especial de fin de cadena
+        indicando que no debe de exceder los 20 espacios establecidos en la constante longCad*/
+        articulo[i].nombreArticulo[longCad]='\0';//el último espacio(21) se guarda para el carácter especial de fin de cadena \0
         cout<<"Introduzca la cantidad que lleva del articulo: ";cin>>articulo[i].cantidad;
         cout<<"Introduzca el precio del articulo: ";cin>>articulo[i].precio;
         cin.ignore(100,'\n');//Es utilizado para quitar el buffer del teclado
     }
-    
 }
 float CostoxArticulo(int n, costoPorArticulo articulo[]){
     for (int i = 0; i < n; i++)
     {
         articulo[i].costoPorArt = (articulo[i].cantidad)*(articulo[i].precio);
-        //El articulo[i].costoPorArt irá almacenando el producto para obtener el costo total del producto
+        //El articulo[i].costoPorArt irá almacenando el producto resultante para obtener el costo total por artículo
     }
 }
 float MostrarInformacion(int n, costoPorArticulo articulo[]){
@@ -37,7 +36,7 @@ float MostrarInformacion(int n, costoPorArticulo articulo[]){
     for (int i = 0; i < n; i++)
     {
         cout<<articulo[i].nombreArticulo<<endl;
-        cout<<articulo[i].cantidad<<" x "<<articulo[i].precio<<" = "<<articulo[i].costoPorArt<<endl;
+        cout<<articulo[i].cantidad<<" x $ "<<articulo[i].precio<<" = $ "<<articulo[i].costoPorArt<<endl;
     }
     
 }
@@ -59,6 +58,6 @@ int main(){
     PedirInformacion(n,Articulo);
     CostoxArticulo(n, Articulo);
     MostrarInformacion(n, Articulo);
-    cout<<"TOTAL A PAGAR: $"<<CostoTotal(n,Articulo);
+    cout<<"TOTAL A PAGAR: $ "<<CostoTotal(n,Articulo);
     return 0;
 }
